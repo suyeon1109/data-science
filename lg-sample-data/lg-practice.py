48f8e0daa05b68a00071b3a10e52a3b4612af4ca
Suyeon Kim <ksy11090@naver.com>
2023-10-24 09:56:09 -0400

    Update lg-practice.py

--- lg-sample-data/lg-practice.py
+++ lg-sample-data/lg-practice.py
@@ -6,35 +6,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("lg-sample-data/Car details v3.csv")
-
+unrelevent = ['name', 'torque', 'mileage','engine','max_power','fuel','owner','transmission','seller_type','max_power']
+df = df.drop(columns=unrelevent)
 df = df.dropna(axis=0)
 
-df['mileage'] = df.mileage.str.split(' ').str[0]
-df['mileage (kmpl)'] = df['mileage'].apply(pd.to_numeric)
+# df['mileage'] = df.mileage.str.split(' ').str[0]
+# df['mileage (kmpl)'] = df['mileage'].apply(pd.to_numeric)
 
-df['engine'] = df.engine.str.split(' ').str[0]
-df['engine (cc)'] = df['engine'].apply(pd.to_numeric)
+# df['engine'] = df.engine.str.split(' ').str[0]
+# df['engine (cc)'] = df['engine'].apply(pd.to_numeric)
 
-df['max_power'] = df.max_power.str.split(' ').str[0]
-df['max_power (bhp)'] = df['max_power'].apply(pd.to_numeric)
-
-unrelevent = ['name', 'torque', 'mileage','engine','max_power']
-df = df.drop(columns=unrelevent)
-
-for each in df['fuel']:
-    if each=='Diesel':
-        df['fuel_weighted'] = 1
-    elif each=="Petrol":
-        df['fuel_weighted'] = 2
-    elif each=="LPG":
-        df['fuel_weighted'] = 3
-    elif each=="CNG":
-        df['fuel_weighted'] = 4
+# df['max_power'] = df.max_power.str.split(' ').str[0]
+# df['max_power (bhp)'] = df['max_power'].apply(pd.to_numeric)
 
 df.info()
 print(df.head(10))
 
-x = df[['year','year', 'selling_price', 'km_driven', 'fuel', 'seller_type', 'transmission', 'owner', 'seats', 'mileage (kmpl)', 'engine (cc)', 'max_power (bhp)']]
+x = df[['year', 'km_driven', 'seats']]
 y = df[['selling_price']]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, test_size=0.2)
@@ -42,7 +30,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, test_s
 model = LinearRegression()
 model.fit(x_train.values, y_train.values)
 
-predict_data = [[2014, 154000, 310, 4, 24.98654, 121.56998]]
+predict_data = [[2014, 154000, 4]]
 predicted = model.predict(predict_data)
 print(predicted)
 
